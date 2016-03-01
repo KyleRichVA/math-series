9d037e34edd0313e50e4f70c5c85ac374f23af22
wohlfea <wohlfea@gmail.com>
2016-03-01 14:49:27 -0800

    sum series function working and tested

--- src/series.py
+++ src/series.py
@@ -10,4 +10,18 @@ def fibonacci(n):
 
 
 def lucas(n):
-    pass
+    if n <= 0:
+        return 0
+    l = [2, 1]
+    while len(l) < n:
+        l.append(l[-1] + l[-2])
+    return l[n-1]
+
+
+def sum_series(n, x=0, y=1):
+    if n <= 0:
+        return 0
+    l = [x, y]
+    while len(l) < n:
+        l.append(l[-1] + l[-2])
+    return l[n-1]
--- src/test_series.py
+++ src/test_series.py
@@ -10,6 +10,25 @@ FIB_NUMS = [
     (7, 8)
 ]
 
+LUCAS_NUMS = [
+    (-3, 0),
+    (0, 0),
+    (1, 2),
+    (2, 1),
+    (3, 3),
+    (4, 4),
+    (5, 7),
+    (7, 18)
+]
+
+SERIES_NUMS = [
+    ((2,), 1),
+    ((3, 2, 1), 3),
+    ((4, 0, 1), 2),
+    ((2, 5, 10), 10),
+    ((3, 5, 5), 10)
+]
+
 
 @pytest.mark.parametrize('n, result', FIB_NUMS)
 def test_fibonacci(n, result):
@@ -17,5 +36,13 @@ def test_fibonacci(n, result):
     assert fibonacci(n) == result
 
 
-def test_lucas():
+@pytest.mark.parametrize('n, result', LUCAS_NUMS)
+def test_lucas(n, result):
     from series import lucas
+    assert lucas(n) == result
+
+
+@pytest.mark.parametrize('args, result', SERIES_NUMS)
+def test_sum_series(args, result):
+    from series import sum_series
+    assert sum_series(*args) == result
