14bab01ec16967e07dd4628fceea88e6e571c8cd
wohlfea <wohlfea@gmail.com>
2016-03-01 13:45:08 -0800

    fibonacci sequence working and passing tests

--- src/series.py
+++ src/series.py
@@ -1,4 +1,9 @@
 
 
-def fibonacci():
-    pass
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    l = [0, 1]
+    while len(l) < n:
+        l.append(l[-1] + l[-2])
+    return l[n-1]
--- src/test_series.py
+++ src/test_series.py
@@ -1,4 +1,17 @@
+import pytest
 
 
-def test_fibonacci():
+FIB_NUMS = [
+    (-3, 0),
+    (0, 0),
+    (1, 0),
+    (2, 1),
+    (3, 1),
+    (7, 8)
+]
+
+
+@pytest.mark.parametrize('n, result', FIB_NUMS)
+def test_fibonacci(n, result):
     from series import fibonacci
+    assert fibonacci(n) == result
